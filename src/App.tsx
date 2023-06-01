48c5132e068fe64140584b087e6e6b266ef3cfbf
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-06-02 00:32:14 +0530

    1. added styles for task list,  learnt percent margin and  padding good for responsiveness,  2. learnt - button=submit causes form refresh on rerender   but not normal button  3. submit button refresh can be avoide using e.preventDefault()  4. ued inputRef for fetching inputfield value  5. rendering tasks in tasklist  if task is not '' or tasklist is not empty  6. handleAddTask prop type with =<>void means return type is void  giving e as React.FormEvent type

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import {SyntheticEvent} from 'react'
 import { log } from 'console';
 
+type Task={
+  id: number,
+  task: string,
+  isDone: boolean
+}
 const App: React.FC = () => {
   const [task, setTask] = useState<string>('')
+  const [taskList, setTaskList] = useState<Task[]>([])
 
-  console.log(task,'added');
+const handleAddTask = (task: string) => {
+
+     if(task!==''){
+      setTask(task);
+      setTaskList(prev=>[...prev, {id:(prev.length+1),task:task,isDone:false}]);
+       }
+  
+ // console.log('Event',e.target.form[0].value)//works with e: any only
+  // const {task} = e.currentTarget.elements.item//.currentTarget.elements.input__text.value
+  //     setTask(task)
+  //     setTaskList(prev=>[...prev,{id:prev.length+1,task:task,isDone:false}])
+}
   
   return (
     <div className="App">
    <span className='heading'> Task List</span>
-   <InputField task={task} callback={setTask}/>
+   <InputField task={task}  handleAddTask={handleAddTask}/>
+   {(task!=='' && taskList) && (
+    <>
+    {console.log(taskList)}
+    {taskList.map((task)=>(
+          <div className="task__list" key={task.id}>
+            <p  className="task__list__id">{task.id}.   </p>
+            <p  className="task__list__taskinfo">{task.task}</p>
+            <p  className="task__list__isDone">{task.isDone}</p>
+          </div>
+    ))}
+    </>
+   )}
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import './InputField.css'
+import React, { useRef } from 'react';
+import './InputField.css';
 
 type Props = {
-    task: string;
-    callback: React.Dispatch<React.SetStateAction<string>>;//type for setState
-}
-const InputField: React.FC<Props> = ({task, callback}) => {
+  task: string;
+  handleAddTask: (task: string) => void;
+};
+
+const InputField: React.FC<Props> = ({ task, handleAddTask }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    handleAddTask(inputRef.current?.value || '');
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  };
+
   return (
     <div className="todo__input">
-      <form className="todo__form">
-        <input type="text"  placeholder="Enter a Task.." className="input__box"
-         onChange={e=>callback(e.target.value)}
-         value={task}
-         />
-        <button type="submit" className="submit__button">Add</button>
+      <form className="todo__form" onSubmit={handleSubmit}>
+        <input
+          name="input__text"
+          type="text"
+          placeholder="Enter a Task.."
+          className="input__box"
+          ref={inputRef}
+        />
+        <button name="button__submit" type="submit" className="submit__button">
+          Add
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
