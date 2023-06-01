1f1d91c5b892d9d824747e7cd1844db3f46c4c93
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-06-01 20:15:08 +0530

    1. passing props to  inputfield comp  2. creating props type   and calling callback oninputfield change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { log } from 'console';
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>('')
+
+  console.log(task,'added');
+  
   return (
     <div className="App">
    <span className='heading'> Task List</span>
-   <InputField/>
+   <InputField task={task} callback={setTask}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './InputField.css'
-const InputField = () => {
+
+type Props = {
+    task: string;
+    callback: React.Dispatch<React.SetStateAction<string>>;//type for setState
+}
+const InputField: React.FC<Props> = ({task, callback}) => {
   return (
     <div className="todo__input">
-      <form action="">
-        <input type="text"  placeholder="Enter a Task.." className="input__box"/>
+      <form className="todo__form">
+        <input type="text"  placeholder="Enter a Task.." className="input__box"
+         onChange={e=>callback(e.target.value)}
+         value={task}
+         />
         <button type="submit" className="submit__button">Add</button>
       </form>
     </div>
