134d920e7f9c320c33974f6c52fd656055075237
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-06-02 14:54:50 +0530

    1. used span instead of p tag for task text for alignment proper with icons 2. edit icon replace with editdone icon conditionally 3. added isedit property to task type 4. handleedittask modify tasklist with new task object having isedit:true using map to preserve order 5. using task.isedit? render input box

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,52 @@ import './App.css';
 import InputField from './components/InputField';
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
-
+import {IoMdCloudDone} from 'react-icons/io'
 type Task = {
   id: number;
   task: string;
   isDone: boolean;
+  isEdit:boolean;
 };
 
 const App: React.FC = () => {
   const [task, setTask] = useState<string>('');
   const [taskList, setTaskList] = useState<Task[]>([]);
-
+  
   const handleAddTask = (task: string) => {
     if (task !== '') {
       setTask(task);
       setTaskList((prev) => [
         ...prev,
-        { id: prev.length + 1, task: task, isDone: false },
+        { id: prev.length + 1, task: task, isDone: false, isEdit:false },
       ]);
     }
   };
+  const handleEditTask = (task:Task)=>{
+    console.log('edit task',task)
+    /** won't work as we r workign on a separate task, but already rendering part of this task is done
+     * so we need to work on State variables to reflect render changes 
+     * meaning need to do state update in tasklist
+    setisEdit(true)
+    task.isEdit= !task.isEdit
+    */
+    setTaskList(prev=>{
 
+     let newList = prev.map(item=>{
+      if(item.id===task.id){
+        return {...task,isEdit:!task.isEdit}
+      }
+      else return item
+
+      })
+      return [...newList]
+    })
+  
+  }
+
+  const handleDoneInputTask = () => {
+  {/**on done icon click the input box replaced by sspan text */}
+  }
   return (
     <div className="App">
       <span className="heading">Task List</span>
@@ -35,7 +60,15 @@ const App: React.FC = () => {
             <div className="task__list" key={task.id}>
               <p className="task__list__id">{task.id}</p>
               <div className="task__list__content">
-                <p className="task__list__taskinfo">{task.task}</p>
+              {
+                task.isEdit?
+                <input name='task-edit' type='text' className='task-input-edit'/>:
+              task.isDone?
+                //  {/**replace edit with done icon */}
+               <s className="task__list__taskinfo">{task.task}</s>:
+               <span className="task__list__taskinfo">{task.task}</span>
+               //when using p tag it aligns the iconsa little above horizontal axis
+               }
                 <div className="icons">
                   <span className="icons__delete" onClick={()=>{
                       setTaskList((prev)=>{
@@ -46,15 +79,35 @@ const App: React.FC = () => {
                 }>
                     <AiFillDelete />
                   </span>
-                  <span className="icons__edit">
-                    <AiFillEdit />
+                  <span className="icons__edit" onClick={()=>{handleEditTask(task)}}>
+                 {task.isEdit?
+                  <IoMdCloudDone/>:
+                    <AiFillEdit />}
                   </span>
                   <span className="icons__done">
-                    <MdDone />
+                    <MdDone onClick={()=>{
+                     setTaskList((prev)=>{
+    /* order won't be preserved but striked item wil be added in the end
+                      let newList = prev.filter(item=>item.id!==task.id)
+                      prev = newList
+                      return [...prev, {id:task.id,task:task.task,isDone:!task.isDone}]
+     */
+    /* using map will preserve the order
+     */
+                      const newList = prev.map(item=>{
+                        if(item.id===task.id){
+                          return {id:task.id,task:task.task,isDone:!task.isDone, isEdit:false} 
+                          // add the current rendered item with isdone switched
+                        }
+                        return item //add the activvely looped items
+                         })
+    
+                           return newList;
+                         })
+                     }}/>
                   </span>
                 </div>
               </div>
-              <p className="task__list__isDone"></p>
             </div>
           ))}
         </>
