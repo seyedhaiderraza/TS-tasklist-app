5f6ab636679780c0075d6a932b92fbec026910c8
seyedhaiderraza <seyedhaiderraza@gmail.com>
2023-07-16 21:55:18 +0530

    fixed misalignment when dragging by removing task__list margin-left

--- src/components/CompletedTasksComponent.tsx
+++ src/components/CompletedTasksComponent.tsx
@@ -2,22 +2,7 @@ import React from 'react'
 
 const CompletedTasksComponent = () => {
   return (<>
-    <div>
-      completed
-
-    </div>
-    <div>
-      completed
-    </div>
-    <div>
-      completed
-    </div><div>
-      completed
-    </div><div>
-      completed
-    </div><div>
-      completed
-    </div>
+    
     </>)
 }
 
